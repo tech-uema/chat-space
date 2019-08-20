1aaf647ee1a874e3f5fad277fbd370aa394047ca
tech-uema <snow_clover495@yahoo.co.jp>
2019-08-20 19:16:50 +0900

    add_group_create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,16 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -11,6 +18,15 @@ class GroupsController < ApplicationController
 
   def update
   end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
+  end
+  
+
+
+
 
 
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,4 @@
 class Group < ApplicationRecord
-  has_many :users through:groups_users
-  has_many :messages
+  has_many :group_users
+  has_many :users, through: :group_users
 end
