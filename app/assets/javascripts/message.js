e20291b61474f0e5614dd214586b6793d03fd068
tech-uema <snow_clover495@yahoo.co.jp>
2019-08-23 19:07:41 +0900

    view_json

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,30 @@
 $(document).on('turbolinks:load', function(){
-  
+
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="message__detail">
+                    <p class="message__detail__current-user-name">
+                      ${message.user_name}
+                    </p>
+                    <p class="message__detail__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <p class="message_body">
+                    <div>
+                    ${content}
+                    </div>
+                    ${img}
+                  </p>
+                </div>`
+  return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    var url = $(this).attr('action')
+    var message = new FormData(this);
+    var url = $(this).attr('action');
     $.ajax({  
       url: url,
       type: 'POST',
@@ -10,6 +32,14 @@ $(document).on('turbolinks:load', function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.form__message').val('') 
+    })
+    .fail(function(data){
+      alert('エラーです');
     })
   })
 });
