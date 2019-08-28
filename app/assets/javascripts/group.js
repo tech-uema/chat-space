6818f33d25a4776479dc4bd0559b9094d101d530
tech-uema <snow_clover495@yahoo.co.jp>
2019-08-28 21:37:25 +0900

    add_user_and_delete

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,57 +1,68 @@
 $(document).on('turbolinks:load', function(){
-
-  function buildHTML(users) {
-   var html = `<div class='chat-group-user'>
-                <input name='group[user_ids][]' type='hidden' value='ユーザーのid'>
-                <p class='chat-group-user__name'>${user.name}</p>
-                <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
-               </div>`
-    
-  }
-
-  $(function() {
-    $('#user-search-field').on("keyup", function() {
-      // var input = $('#user-search-result').val();
-      console.log("fire ok");
-    });
-  });
+ $(function() {
 
 
-  $('#user-search-field').on('keyup', function(){
-    
-    var users = $('#user-search-field').val();
-    var url = "/users";
-    $.ajax({  
-      url: url,
-      type: 'GET',
-      data: { name: users },
-      dataType: 'json',
-      processData: false,
-      contentType: false
+    function buildHTML(user) {
+      var html = `<div class="chat-group-user clearfix" id="user_namber${user.name}">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" id='add-btn' data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                  </div>`
+      return html;
+    }
+
+
+    function addGroup(user) {
+      var html = `<div class='chat-group-user'>
+                  <input name='group[user_ids][]' type='hidden' value='${user.id}'>
+                  <p class='chat-group-user__name'>${user.name}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                  </div>`
+      return html;
+    }
+
+    $('#user-search-field').on('keyup', function(){
+      var input = $('#user-search-field').val();
+      var url = '/users';
+      $.ajax({  
+        url: url,
+        type: 'GET',
+        data: { keyword: input },
+        dataType: 'json',
+        processData: false,
+        contentType: false
+        })
+
+      .done(function(users) {
+        $("#user-search-result").empty();
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            var html = buildHTML(user)
+            $('#user-search-result').append(html)
+          });
+        }
+        else {
+          // appendErrMsgToHTML("一致する映画はありません");
+        }
+        })
+        .fail(function() {
+          alert('ユーザー検索に失敗しました');
+        })
+  　})
+
+    $(document).on('click','#add-btn',function(){
+      var user = {}
+      user.id = $(this).attr('data-user-id');
+      user.name =$(this).attr('data-user-name');
+      var html = addGroup(user)
+      $('#goup-menber').append(html);
+      $(`#user_namber${user.name}`).empty();
+      
     })
 
-    .done(function(users) {
-      if (users.length !== 0) {
-        users.forEach(function(users){
-          buildHTML(users);
-        });
-      }
-      else {
-        appendErrMsgToHTML("一致する映画はありません");
-      }
+    $(document).on('click','.js-remove-btn',function(){
+      console.log("fire")
+      $(this).parent('div').remove();
     })
 
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('#new_message')[0].reset();
-      scrollBottom();
-
-    })
-
-
-
-
-
-  })
+ });
 });
--- app/views/users/index.json.jbuilder
+++ app/views/users/index.json.jbuilder
@@ -1,4 +1,4 @@
-json.array! @users do |users|
-  # json.id user.id
-  json.name users.name
+json.array! @users do |user|
+  json.id user.id
+  json.name user.name
 end
