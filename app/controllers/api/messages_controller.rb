0ab40493a7bcf397494924a7794aa4583f4caba4
tech-uema <snow_clover495@yahoo.co.jp>
2019-08-29 18:08:43 +0900

    Update messages_controller.rb

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,5 @@
 class Api::MessagesController < ApplicationController
   def index
-    @group = Group.find(params[:group_id])
-    @messages = @group.messages.includes(:user).where('id > ?', params[:last_id])
+    @messages = Message.where('id > ? and group_id = ?', params[:last_id], params[:group_id]).includes(:user)
   end
 end
